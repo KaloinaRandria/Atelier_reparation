e3ba453d5877207a9a547a2f6798af4578c99fa3
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-12-19 07:23:05 +0300

    initialize project

---
+++ src/main/java/mg/working/atelier_reparation/AtelierReparationApplication.java
@@ -0,0 +1,13 @@
+package mg.working.atelier_reparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierReparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierReparationApplication.class, args);
+	}
+
+}
