1230d8aa000364337173fdc2db2fe1c30ae34f21
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-01-11 20:07:25 +0300

    Update MCD-atelier.sql

--- MCD-atelier.sql
+++ MCD-atelier.sql
@@ -27,17 +27,6 @@ CREATE TABLE modele(
    FOREIGN KEY(id_marque) REFERENCES marque(id_marque)
 );
 
-CREATE TABLE ordinateur(
-   id_ordinateur SERIAL,
-   num_serie VARCHAR(255)  NOT NULL,
-   id_modele INTEGER NOT NULL,
-   id_client INTEGER NOT NULL,
-   PRIMARY KEY(id_ordinateur),
-   UNIQUE(num_serie),
-   FOREIGN KEY(id_modele) REFERENCES modele(id_modele),
-   FOREIGN KEY(id_client) REFERENCES client(id_client)
-);
-
 CREATE TABLE Mvt_stock(
    id_mvt_stock SERIAL,
    libelle VARCHAR(255) ,
@@ -45,15 +34,22 @@ CREATE TABLE Mvt_stock(
    PRIMARY KEY(id_mvt_stock)
 );
 
-CREATE TABLE Mvt_stock_fille(
-   id_mvt_stock_fille SERIAL,
-   entree INTEGER,
-   sortie INTEGER,
-   id_ordinateur INTEGER NOT NULL,
-   id_mvt_stock INTEGER NOT NULL,
-   PRIMARY KEY(id_mvt_stock_fille),
-   FOREIGN KEY(id_ordinateur) REFERENCES ordinateur(id_ordinateur),
-   FOREIGN KEY(id_mvt_stock) REFERENCES Mvt_stock(id_mvt_stock)
+CREATE TABLE composant(
+   id_composant SERIAL,
+   libelle VARCHAR(255)  NOT NULL,
+   PRIMARY KEY(id_composant)
+);
+
+CREATE TABLE type_ordinateur(
+   id_type_ordinateur SERIAL,
+   libelle VARCHAR(255)  NOT NULL,
+   PRIMARY KEY(id_type_ordinateur)
+);
+
+CREATE TABLE type_reparation(
+   id_type_reparation SERIAL,
+   libelle VARCHAR(255)  NOT NULL,
+   PRIMARY KEY(id_type_reparation)
 );
 
 CREATE TABLE technicien(
@@ -67,15 +63,43 @@ CREATE TABLE technicien(
    FOREIGN KEY(id_specialite) REFERENCES specialite(id_specialite)
 );
 
+CREATE TABLE ordinateur(
+   id_ordinateur SERIAL,
+   num_serie VARCHAR(255)  NOT NULL,
+   id_type_ordinateur INTEGER NOT NULL,
+   id_modele INTEGER NOT NULL,
+   id_client INTEGER NOT NULL,
+   PRIMARY KEY(id_ordinateur),
+   UNIQUE(num_serie),
+   FOREIGN KEY(id_type_ordinateur) REFERENCES type_ordinateur(id_type_ordinateur),
+   FOREIGN KEY(id_modele) REFERENCES modele(id_modele),
+   FOREIGN KEY(id_client) REFERENCES client(id_client)
+);
+
 CREATE TABLE reparation(
    id_reparation SERIAL,
    desc_probleme VARCHAR(255)  NOT NULL,
    date_depot DATE NOT NULL,
    date_retrait DATE,
    cout_reparation NUMERIC(15,2)   NOT NULL,
+   id_type_reparation INTEGER NOT NULL,
+   id_composant INTEGER NOT NULL,
    id_technicien INTEGER NOT NULL,
    id_ordinateur INTEGER NOT NULL,
    PRIMARY KEY(id_reparation),
+   FOREIGN KEY(id_type_reparation) REFERENCES type_reparation(id_type_reparation),
+   FOREIGN KEY(id_composant) REFERENCES composant(id_composant),
    FOREIGN KEY(id_technicien) REFERENCES technicien(id_technicien),
    FOREIGN KEY(id_ordinateur) REFERENCES ordinateur(id_ordinateur)
+);
+
+CREATE TABLE Mvt_stock_fille(
+   id_mvt_stock_fille SERIAL,
+   entree INTEGER,
+   sortie INTEGER,
+   id_ordinateur INTEGER NOT NULL,
+   id_mvt_stock INTEGER NOT NULL,
+   PRIMARY KEY(id_mvt_stock_fille),
+   FOREIGN KEY(id_ordinateur) REFERENCES ordinateur(id_ordinateur),
+   FOREIGN KEY(id_mvt_stock) REFERENCES Mvt_stock(id_mvt_stock)
 );
