106e28314343f3ae253b081ccf0994a8208244ab
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-01-12 17:00:48 +0300

    Update OrdinateurController.java

--- src/main/java/mg/working/atelier_reparation/controller/OrdinateurController.java
+++ src/main/java/mg/working/atelier_reparation/controller/OrdinateurController.java
@@ -1,23 +1,45 @@
 package mg.working.atelier_reparation.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import mg.working.atelier_reparation.model.Technicien;
 import mg.working.atelier_reparation.model.materiel.Ordinateur;
+import mg.working.atelier_reparation.model.util.Composant;
+import mg.working.atelier_reparation.model.util.TypeReparation;
+import mg.working.atelier_reparation.services.TechnicienService;
 import mg.working.atelier_reparation.services.materiel.OrdinateurService;
+import mg.working.atelier_reparation.services.util.ComposantService;
+import mg.working.atelier_reparation.services.util.TypeReparationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class OrdinateurController {
     @Autowired
     OrdinateurService ordinateurService;
+    @Autowired
+    TechnicienService technicienService;
+    @Autowired
+    TypeReparationService typeReparationService;
+    @Autowired
+    ComposantService composantService;
 
     @GetMapping("/ordinateur/reparation")
     public String goToReparationOrdinateur(HttpServletRequest request,@RequestParam(name = "ordinateur") String ordinateur) {
         Ordinateur ordinateur1 = this.ordinateurService.findOrdinateurById(ordinateur);
         ordinateur1.setIsDiagnostic(true);
 
-        return "";
+        List<Technicien> techniciens = this.technicienService.listTechnicien();
+        request.setAttribute("techniciens", techniciens);
+
+        List<TypeReparation> typeReparations = this.typeReparationService.getAllTypeReparation();
+        request.setAttribute("typeReparations", typeReparations);
+
+        List<Composant> composants = this.composantService.getComposants();
+        request.setAttribute("composants", composants);
+        return "/home/reparation/insertReparation";
     }
 }
